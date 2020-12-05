b31ea30fe17dff7a989768028e314a7e1b7c6bdb
Mr-zhong7 <2447363357@qq.com>
2020-12-05 17:05:57 +0800

    init waimai

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import ajax from './ajax'
+
+// 根据经纬度获取位置详情
+export const reqAddress=(geohash)=>ajax(`/position/${geohash}`)
+// 获取食品分类列表
+export const reqFoodType=()=>ajax('/index_category')
+// 根据经纬度获取商铺列表
+export const reqShops=(longitude,latitude)=>ajax('/shops',{longitude,latitude}
